793f164017b5a5d4d17e1d0d5528e71f103bec89
yevheniiredziuk <yevheniiredziuk@gmail.com>
2025-09-06 09:50:38 +0300

    fix Spring starters versions to 3.5.4

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,15 +1,23 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
 
     public User(){
     }
+
 }
 
