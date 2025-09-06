ba84ec6a0ae0b7cca2db2ffa9ea269edcba1eda8
yevheniiredziuk <yevheniiredziuk@gmail.com>
2025-09-06 22:45:44 +0300

    add login page and permissions to all endpoints for all users

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,7 +16,22 @@ public class SecurityConfig  {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/users/**").permitAll ());
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/users/**")
+                .authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                );
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                //.roles("USER")
+                .build();
+        return  new InMemoryUserDetailsManager(user);
     }
 }
